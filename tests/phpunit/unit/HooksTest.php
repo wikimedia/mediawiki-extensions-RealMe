4a0f1872996571fa70079dacdd4bd097485f16e3
Taavi Väänänen <hi@taavi.wtf>
2022-11-18 10:16:46 +0200

    Initial commit

---
+++ tests/phpunit/unit/HooksTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MediaWiki\Extension\RelMe\Tests;
+
+use MediaWikiUnitTestCase;
+
+/**
+ * @coversDefaultClass \MediaWiki\Extension\RelMe\Hooks
+ */
+class HooksTest extends MediaWikiUnitTestCase {
+
+}
