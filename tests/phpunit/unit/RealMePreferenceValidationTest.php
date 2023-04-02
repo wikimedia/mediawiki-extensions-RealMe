30c696879ff9a2132297a073795bb93176a0789b
Taavi Väänänen <hi@taavi.wtf>
2023-04-02 13:33:48 +0300

    Add validation for preference contents
    * Added a limit on how many URLs can be set as verified, with an
      arbitrarily picked default value of five URLs per user. We can
      re-visit the max limit if the current value proves insufficient.
    * Validate that all values specified are valid HTTP or HTTPS URLs.
      Protocol-relative URLs are not allowed because they feel wrong to me.
    
    Change-Id: I54acb4e473f33b4c244ae4039c4ea9c24cd8c990
    (cherry picked from commit 992a10bc9d1d8723e57a9bd177f83640bef5c8e4)

---
+++ tests/phpunit/unit/RealMePreferenceValidationTest.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+use MediaWiki\Extension\RealMe\Constants;
+use MediaWiki\Extension\RealMe\Hooks;
+use MediaWiki\Tests\Unit\FakeQqxMessageLocalizer;
+use MediaWiki\User\UserFactory;
+use MediaWiki\User\UserOptionsLookup;
+use MediaWiki\Utils\UrlUtils;
+
+/**
+ * @author Taavi Väänänen <hi@taavi.wtf>
+ */
+class RealMePreferenceValidationTest extends MediaWikiUnitTestCase {
+	private function getValidationCallback(): callable {
+		$options = [];
+
+		( new Hooks(
+			new HashConfig( [ 'RealMeUserPageUrlLimit' => 5 ] ),
+			new UrlUtils(),
+			$this->createNoOpMock( UserFactory::class ),
+			$this->createNoOpMock( UserOptionsLookup::class ),
+		) )
+			->onGetPreferences(
+				$this->createNoOpMock( User::class ),
+				$options
+			);
+
+		return $options[Constants::PREFERENCE_NAME]['validation-callback'];
+	}
+
+	private function getMockForm(): HTMLForm {
+		$mockForm = $this->createMock( HTMLForm::class );
+		$mockForm->method( 'msg' )->willReturnCallback( [ new FakeQqxMessageLocalizer(), 'msg' ] );
+		return $mockForm;
+	}
+
+	/**
+	 * @param string $value
+	 * @dataProvider provideValidPreferences
+	 * @covers \MediaWiki\Extension\RealMe\Hooks::onGetPreferences
+	 */
+	public function testPreferenceValidationValid( string $value ) {
+		$callback = $this->getValidationCallback();
+		$result = $callback( $value, [], $this->getMockForm() );
+		$this->assertTrue( $result );
+	}
+
+	public static function provideValidPreferences(): iterable {
+		yield 'Empty' => [ '' ];
+		yield 'One URL' => [ "https://example.com\n" ];
+		yield 'Five URLs' => [
+			"https://one.example.com\nhttps://two.example.com\nhttps://three.example.com"
+			. "\nhttps://four.example.com\nhttps://five.example.com\n"
+		];
+		yield 'Empty lines between URLs' => [ "https://example.com\n\nhttps://two.example.com\n" ];
+		yield 'Plaintext HTTP works too' => [ "http://example.com\n" ];
+	}
+
+	/**
+	 * @param string $value
+	 * @dataProvider provideInvalidPreferences
+	 * @covers \MediaWiki\Extension\RealMe\Hooks::onGetPreferences
+	 */
+	public function testPreferenceValidationInvalid( string $value ) {
+		$callback = $this->getValidationCallback();
+		$result = $callback( $value, [], $this->getMockForm() );
+		$this->assertIsArray( $result, 'List of errors should be an array' );
+	}
+
+	public static function provideInvalidPreferences(): iterable {
+		yield 'Not an URL' => [ "bananas\n" ];
+		yield 'Six URLs' => [
+			"https://one.example.com\nhttps://two.example.com\nhttps://three.example.com"
+			. "\nhttps://four.example.com\nhttps://five.example.com\nhttps://six.example.com\n"
+		];
+		yield 'Mixed URLs and non-urls' => [ "https://one.example.com\nnope\nhttps://six.example.com\n" ];
+		yield 'Wrong protocol in URL' => [ "ftp://one.example.com\n" ];
+		yield 'Protocol-relative URL' => [ "//one.example.com\n" ];
+	}
+}
